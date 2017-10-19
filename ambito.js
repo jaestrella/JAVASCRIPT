abe0a48f2ee0120f9a2a7bce5dfaea4ef970d417
jaestrella <jaestrella1991@gmail.com>
2017-10-19 12:21:28 +0200

    nuevo

--- ambito.js
+++
@@ -1,8 +0,0 @@
-var a = 1;
-var b = 2;
-var funcion=function(){
-var c=3;
-return c;
-}
-
-console.log(a,b,funcion());
--- funcion.js
+++
@@ -1,6 +0,0 @@
-var incrementar=(function(){
-var contador=0;
-return function(){return contador++};
-})();
-console.log(incrementar());
-
